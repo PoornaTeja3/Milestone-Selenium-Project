8598b879bf295c7167a205cc54c5ce97bf0fcdd5
Poorna Teja Karepati <poornatejak333@gmail.com>
2026-03-09 12:04:56 +0530

    Fixed chromeDriver version issue for Jenkins

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -18,7 +19,11 @@ public class BaseTest {
 
         WebDriverManager.chromedriver().setup();
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
 
